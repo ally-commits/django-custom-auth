6089d304c5f81b8409829d98a5fd0f0f2177f45b
AlwinCrasta <alwyncrasta000@gmail.com>
2021-02-15 22:28:31 +0530

    dashbaord design init logout design

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index,name="homePage"),
+    path('',views.index,name="home"),
     path('accounts/sign_up/',views.sign_up,name="sign-up")
 ]
